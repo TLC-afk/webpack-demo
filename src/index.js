11308c3c82985b3d83b15a1ae197d82b09ccfce6
Mr.tang <1261651023@qq.com>
2021-08-13 10:46:09 +0800

    准备部署

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
-import  './x.css'
+import  './x.scss'
+import './y.less'
+import './z.styl'
+import jpg from './assets/1.jpg'
+
+const div = document.getElementById("xxx")
+div.innerHTML = `<img src="${jpg}">`
+
+//懒加载
+const button = document.createElement('button')
+button.innerText ='懒加载'
+div.appendChild(button)
+button.onclick = ()=>{
+   const promise = import('./lazy.js')
+   promise.then((module) => {
+       const fn = module.default
+       fn()
+   },() => {
+       console.log('模块加载错误')
+   })
+}
--- webpack.config.base.js
+++ webpack.config.base.js
@@ -11,6 +11,47 @@ module.exports = {
     new HtmlWebpackPlugin({
       title: "chen",
       template: "src/assets/test.html",
-    })
+    }),
   ],
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: "file-loader",
+          },
+        ],
+      },
+      {
+        test: /\.styl$/,
+        loader: ["style-loader", "css-loader", "stylus-loader"],
+      },
+      {
+        test: /\.less$/i,
+        loader: [
+          // compiles Less to CSS
+          "style-loader",
+          "css-loader",
+          "less-loader",
+        ],
+      },
+      {
+        test: /\.scss$/i,
+        use: [
+          // 将 JS 字符串生成为 style 节点
+          "style-loader",
+          // 将 CSS 转化成 CommonJS 模块
+          "css-loader",
+          // 将 Sass 编译成 CSS
+          {
+            loader: "sass-loader",
+            options: {
+              implementation: require("dart-sass"),
+            },
+          },
+        ],
+      },
+    ],
+  },
 };
